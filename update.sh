0ebf444bce1924f077ce62fe2452396db932bba2
Ohidur Rahman Bappy <22405409+ohidurbappy@users.noreply.github.com>
2023-08-19 15:55:52 +0000

    added script

---
+++ update.sh
@@ -0,0 +1,8 @@
+pip install git+https://gitlab.com/fdroid/fdroidserver.git
+sudo apt update && sudo apt install android-sdk
+
+sudo apt install git-restore-mtime 
+git restore-mtime
+
+cd fdroid
+fdroid update --create-metadata
